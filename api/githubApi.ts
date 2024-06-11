f5f8dcf41d062a82855fabac8a454240ccb5f65e
Feras Ahmad <firastaf878@gmail.com>
2024-06-11 13:14:54 -0400

    fix: error coming from githubKey

--- api/githubApi.ts
+++ api/githubApi.ts
@@ -1,13 +1,31 @@
 import axios from "axios";
+import Constants from "expo-constants";
+
+type ExpoConfig = {
+  extra?: {
+    githubKey?: string;
+  };
+};
+
+const { githubKey } = (Constants.expoConfig as ExpoConfig)?.extra ?? {};
+
+if (!githubKey) {
+  throw new Error("GitHub API key is not defined in expo constants.");
+}
 
 const apiClient = axios.create({
   baseURL: "https://api.github.com",
   headers: {
-    Authorization: `token ${process.env.GITHUB_TOKEN}`,
+    Authorization: `token ${githubKey}`,
   },
 });
 
 export const fetchRepos = async (query: string) => {
   const response = await apiClient.get(`/search/repositories?q=${query}`);
   return response.data.items;
+};
+
+export const fetchRepoLanguages = async (languagesUrl: string) => {
+  const response = await apiClient.get(languagesUrl);
+  return response.data;
 };
