97c5d0bddeb2095a958174d7a3fd8906541037dd
Feras Ahmad <firastaf878@gmail.com>
2024-06-11 13:16:11 -0400

    new: added colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -2,6 +2,7 @@ export const colors = {
   text: "rgb(0,0,0)",
   pink: "rgb(227,220,240)",
   white: "rgb(241,241,241)",
-  tint: "#0a7ea4",
-  icon: "rgb(15,15,15)",
+  gray: "rgb(112,112,112)",
+  darkGray: "rgb(27,27,27)",
+  blue: "rgb(32, 111, 235)",
 };
